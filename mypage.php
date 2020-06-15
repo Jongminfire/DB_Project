6a0b6a483afc2794110acf4693850cb7e30adb05
junhomoon <junhomoon224@gmail.com>
2020-06-16 01:40:12 +0900

    0140

--- mypage.php
+++ mypage.php
@@ -18,8 +18,15 @@ if($_SESSION['id']!=null) {
         width: 100%;
       }
 
-      td, th
+      td
       {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+      }
+      th
+      {
+        background-color: #AAAAFF;
         border: 1px solid #dddddd;
         text-align: left;
         padding: 8px;
@@ -38,41 +45,68 @@ if($_SESSION['id']!=null) {
       {
   			float: left;
   		}
-      /* Create two equal columns that floats next to each other */
-      .split
+      p
       {
-        height: 100%;
-        width: 50%;
-        position: fixed;
-        overflow-x: hidden;
-        padding-top: 20px;
-      }
-
-      .left
-      {
-        left: 0;
-      }
-
-      .right
-      {
-        right: 0;
+        margin: 30px;
+        margin-top:50px; 
+        margin-bottom: 50px;
       }
     </style>
   </head>
-<table>
-    <tr>
-      <td>아이디</td>
-      <td>비밀번호</td>
-      <td>학과</td>
-      <td>학년</td>
-    </tr>
-    <tr>
-      <td><? echo "$user_id";?></td>
-      <td><? echo "$user_pwd";?></td>
-      <td><? echo "$dept_name";?></td>
-      <td><? echo "$user_grade";?></td>
-    </tr>
-  </table>
+  <body>
+    <p>
+      <table><h1>회원 정보</h1>
+        <tr>
+          <th>아이디</th>
+          <th>비밀번호</th>
+          <th>학과</th>
+          <th>학년</th>
+        </tr>
+        <tr>
+          <td><? echo "$user_id";?></td>
+          <td><? echo "$user_pwd";?></td>
+          <td><? echo "$dept_name";?></td>
+          <td><? echo "$user_grade";?></td>
+        </tr>
+      </table>
+    </p>
+    <p>
+      <table><h1>내가 남긴 평점</h1>
+        <tr>
+          <th>과목명</th>
+          <th>교수님</th>
+          <th>평점</th>
+          <th>코멘트</th>
+        </tr>
+        <tr>
+          <td><? echo "$user_id";?></td>
+          <td><? echo "$user_pwd";?></td>
+          <td><? echo "$dept_name";?></td>
+          <td><? echo "$user_grade";?></td>
+        </tr>
+      </table>
+    </p>
+    <p>
+      <table><h1>내가 담은 과목</h1>
+        <tr>
+          <th>과목명</th>
+          <th>교수님</th>
+          <th>학과</th>
+          <th>학년</th>
+          <th>평점</th>
+          <th>경쟁률</th>
+        </tr>
+        <tr>
+          <td><? echo "$user_id";?></td>
+          <td><? echo "$user_pwd";?></td>
+          <td><? echo "$dept_name";?></td>
+          <td><? echo "$user_grade";?></td>
+          <td><? echo "$user_grade";?></td>
+          <td><? echo "$user_grade";?></td>
+        </tr>
+      </table>
+    </p>
+  </body>
 
 <?php
 }else{
