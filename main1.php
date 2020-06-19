ae7eabbd9cdbebd3caf9ca6f84ca379470fd4b6b
whd9612 <jongminfire@gmail.com>
2020-06-19 21:05:37 +0900

    200619 21:05

--- main1.php
+++ main1.php
@@ -19,7 +19,7 @@
     <form name="login_form" action = './login.php' method = "post">
       <span style="font-size:100px; color:#bd1c11; font-weight:bold;">SEJONG.GG</span><br><br><br><br>
       <label>아이디: </label> <input type = "text" name = "user_id"></br>
-      <label>비밀번호: </label><input type = "password" name = "user_pwd"<br>
+      <label>비밀번호: </label><input type = "password" name = "user_pwd"<br>  <!--보안을 위해서 입력시에 값이 보이지 않게 설정함 -->
       </p>
       <input type = "button" value ="로그인" onClick= "checkform()">
       <input type = "button" value ="회원가입" onClick= "location.href='signup.php'">
--- user_check.php
+++ user_check.php
@@ -1,8 +1,10 @@
 <?
+  // user_check.php: 같은 단과와 더 높은 학년 과목을 제외 하기 위해 user 값들을 받는 php
+
   session_start();
   include './dbconn.php'; //dpconn 내용 중복되지 않게
 
-  $id= $_SESSION['id'];
+  $id= $_SESSION['id'];   //SESSION으로 현재 로그인 되어있는 id 받음.
   $query ="Select * from user where user_id = '$id'";
 
   $result = mysqli_query($conn, $query);
